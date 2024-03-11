1dd688eb2c44069605b6926cabf597ec71ecdc2a
1zumisawashun <shun.1zumisawa@gmail.com>
2024-03-11 17:40:04 +0900

    add unit test

---
+++ app/functions/hooks/useActiveStep/index.spec.ts
@@ -0,0 +1,39 @@
+import { useActiveStep } from "@/functions/hooks/useActiveStep";
+import { act, renderHook } from "@testing-library/react";
+
+describe("useActiveStepの単体テスト", () => {
+  test("初期値が10に設定されている", () => {
+    // arrange
+    const initial = 10;
+    const { result } = renderHook(() => useActiveStep(initial));
+    // assert
+    expect(result.current.activeStep).toBe(10);
+  });
+
+  test("nextを実行するとactiveStepが増える", () => {
+    // arrange
+    const { result } = renderHook(() => useActiveStep(10));
+    // act
+    act(() => result.current.next());
+    // assert
+    expect(result.current.activeStep).toBe(11);
+  });
+
+  test("backを実行するとactiveStepが減る", () => {
+    // arrange
+    const { result } = renderHook(() => useActiveStep(10));
+    // act
+    act(() => result.current.back());
+    // assert
+    expect(result.current.activeStep).toBe(9);
+  });
+
+  test("resetを実行するとactiveStepが0になる", () => {
+    // arrange
+    const { result } = renderHook(() => useActiveStep(10));
+    // act
+    act(() => result.current.reset());
+    // assert
+    expect(result.current.activeStep).toBe(0);
+  });
+});
---
+++ app/functions/hooks/useDisclosure/index.spec.ts
@@ -0,0 +1,39 @@
+import { useDisclosure } from "@/functions/hooks/useDisclosure";
+import { act, renderHook } from "@testing-library/react";
+
+describe("useDisclosureの単体テスト", () => {
+  test("初期値がtrueに設定されている", () => {
+    // arrange
+    const initial = true;
+    const { result } = renderHook(() => useDisclosure(initial));
+    // assert
+    expect(result.current.isOpen).toBe(true);
+  });
+
+  test("openを実行するとtrueになる", () => {
+    // arrange
+    const { result } = renderHook(() => useDisclosure());
+    // act
+    act(() => result.current.open());
+    // assert
+    expect(result.current.isOpen).toBe(true);
+  });
+
+  test("closeを実行するとfalseになる", () => {
+    // arrange
+    const { result } = renderHook(() => useDisclosure());
+    // act
+    act(() => result.current.close());
+    // assert
+    expect(result.current.isOpen).toBe(false);
+  });
+
+  test("toggleを実行するとtrueになる", () => {
+    // arrange
+    const { result } = renderHook(() => useDisclosure());
+    // act
+    act(() => result.current.toggle());
+    // assert
+    expect(result.current.isOpen).toBe(true);
+  });
+});
