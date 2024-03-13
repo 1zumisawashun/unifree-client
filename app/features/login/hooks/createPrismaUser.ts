155818080a1483c3be965ef8f297cb2e70e75f4a
1zumisawashun <shun.1zumisawa@gmail.com>
2024-03-13 18:09:50 +0900

    add direct url

--- app/features/login/hooks/createPrismaUser.ts
+++ app/features/login/hooks/createPrismaUser.ts
@@ -8,6 +8,8 @@ type Props = {
 export async function createPrismaUser({ uid, picture }: Props) {
   const url = API.createPrismaUser
 
+  console.log(uid, '==============================')
+
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify({ uid, photoURL: picture, displayName: null })
