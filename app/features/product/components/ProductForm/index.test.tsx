033da7b7bdea737c5c85c1a1ab66d34a8fac5b7e
1zumisawashun <shun.1zumisawa@gmail.com>
2024-03-22 20:10:10 +0900

    fix product form test

--- app/features/product/components/ProductForm/index.test.tsx
+++ app/features/product/components/ProductForm/index.test.tsx
@@ -1,38 +1,47 @@
-// import { render, screen } from '@/__tests__/utilities/test-utils'
-// import { ProductForm } from '@/features/product/components/ProductForm'
-// import { productEntity } from '@/features/product/product.schema'
+import { render, screen } from '@/__tests__/utilities/test-utils'
+import { ProductForm } from '@/features/product/components/ProductForm'
+import { productEntity } from '@/features/product/product.schema'
 
-// const categoryOptions = [
-//   { value: 1, label: 'category1' },
-//   { value: 2, label: 'category2' }
-// ]
-// describe('test for product-form-component', () => {
-//   // NOTE:普通の要素取得
-//   it('check product create', async () => {
-//     const mockSave = jest.fn()
-//     const { user } = render(
-//       <ProductForm
-//         submit={mockSave}
-//         product={productEntity}
-//         href={'/products'}
-//         domain="作成する"
-//         isPending={false}
-//         categoryOptions={categoryOptions}
-//       />
-//     )
+const options = [
+  { value: 1, label: 'option1' },
+  { value: 2, label: 'option2' },
+  { value: 2, label: 'option3' }
+]
 
-//     const name = screen.getByRole('textbox', { name: '商品名 必須' })
-//     await user.type(name, 'サンプル')
+// FIXME: 暫定なので後で修正
+describe('ProductFormのインテグレーションテスト', () => {
+  it('商品を作成する', async () => {
+    // arrange
+    const handleClick = jest.fn()
+    const { user } = render(
+      <ProductForm
+        submit={handleClick}
+        product={productEntity}
+        href={'/products'}
+        domain="作成する"
+        isPending={false}
+        categoryOptions={options}
+      />
+    )
+    const name = screen.getByRole('textbox', { name: '商品名 必須' })
+    const price = screen.getByRole('textbox', { name: '価格 必須' })
+    const description = screen.getByRole('textbox', { name: '詳細情報 必須' })
+    const status = screen.getByLabelText('販売中')
+    const paymentMethod = screen.getByLabelText('手渡し')
+    const categories = screen.getByLabelText('option1')
+    const submit = screen.getByRole('button')
 
-//     const price = screen.getByRole('textbox', { name: '価格 必須' })
-//     await user.type(price, '1111')
+    // act
+    await user.type(name, 'サンプル')
+    await user.type(price, '1111')
+    await user.type(description, 'サンプル')
+    await user.click(status)
+    await user.click(paymentMethod)
+    await user.click(categories)
 
-//     const description = screen.getByRole('textbox', { name: '詳細情報 必須' })
-//     await user.type(description, 'サンプル')
+    await user.click(submit)
 
-//     const status = screen.getByRole('checkbox', { name: '必須' })
-//     await user.click(status)
-
-//     expect(name).toBeInTheDocument()
-//   })
-// })
+    // assert
+    expect(handleClick).toHaveBeenCalled()
+  })
+})
--- app/functions/helpers/typeGuard.ts
+++ app/functions/helpers/typeGuard.ts
@@ -6,6 +6,8 @@ function isNumber(value: any): value is number {
   return typeof value === 'number'
 }
 
+/** @see https://zenn.dev/oreo2990/articles/5f75eaa285f2f9#5-%E5%9E%8B%E3%82%AC%E3%83%BC%E3%83%89%E3%81%AE%E5%A4%89%E6%95%B0%E4%BB%A3%E5%85%A5 */
+/** TypeScript4.4以降で使用可能な機能になりますが、型ガードに変数を使うことができます。 */
 const isFile = (file: unknown): file is File => {
   return file instanceof File
 }
