e2a6fd04381cfb494ebb4c92d8894f6c40d50ece
1zumisawashun <shun.1zumisawa@gmail.com>
2024-05-04 14:36:30 +0900

    fix modal hookd

--- app/functions/hooks/useDialog/index.ts
+++ app/functions/hooks/useDialog/index.ts
@@ -5,19 +5,8 @@ import { ElementRef, useCallback, useRef } from 'react'
 export const useDialog = () => {
   const ref = useRef<ElementRef<'dialog'>>(null)
 
-  const open = useCallback(() => {
-    if (ref.current) {
-      document.body.style.overflow = 'hidden'
-      ref.current.showModal()
-    }
-  }, [])
-
-  const close = useCallback(() => {
-    if (ref.current) {
-      document.body.style.overflow = 'auto'
-      ref.current.close()
-    }
-  }, [])
+  const open = useCallback(() => ref.current?.showModal(), [])
+  const close = useCallback(() => ref.current?.close(), [])
 
   return { ref, open, close }
 }
