952b525615b0007afefa916f393adc4e87e57488
1zumisawashun <shun.1zumisawa@gmail.com>
2024-06-11 22:08:43 +0900

    fix

--- app/components/samples/Accordion/index.tsx
+++ app/components/samples/Accordion/index.tsx
@@ -15,7 +15,7 @@ export const Accordion: React.FC = () => {
   return (
     <>
       <section aria-labelledby="title">
-        <h1 id="title">アコーディオンの実装例テストテストテストテストテストテスト</h1>
+        <h1 id="title">アコーディオンの実装例</h1>
         <details
           className={styles.accordion}
           name="sample"
